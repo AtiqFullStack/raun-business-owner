75eae6a20749398b3fe94dee4d9640fbd7887d53
atiqFullstack <atiq.fzeetechz@gmail.com>
2026-09-01 14:19:50 +0530

    -app renamed

--- android/app/src/main/java/com/raun/bisnessowner/MainApplication.kt
+++ android/app/src/main/java/com/raun/bisnessowner/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.raun
+package com.raun.bisnessowner
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/BusinessOwner/AppDelegate.swift
+++ ios/BusinessOwner/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "raun",
+      withModuleName: "Business Owner",
       in: window,
       launchOptions: launchOptions
     )
